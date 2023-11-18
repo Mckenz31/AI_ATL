759878e1bebb62baff9538c65317f1656a5f700f
moisedk <moisedete@gmail.com>
2023-11-18 09:35:14 -0500

    Change key-value quiz to json objects

--- backend/utils.py
+++ backend/utils.py
@@ -1,25 +1,28 @@
 
+import json
 # Takes some quiz questions - response and process to form two dictionaries of commons keys where values are questions and responses respctively
-def process_quiz(quiz_content):
-    lines = []
-    questions = {}
-    responses = {}
-    index  = 1
-    with open (quiz_content, "r") as quiz_content:
-        lines = quiz_content.readlines()
-        index = 1
-        while index < len(lines):
-            question = lines[index - 1]
-            quiz_content = lines[index]
-            questions[index] = question.removeprefix("Question {index}: ")
-            responses[index] = quiz_content.removeprefix("Question {index}: ")
-            index += 3
-    return [questions,responses]
+def process_quiz(quiz_content:str):
+    json_arr = []
+    content_parts = quiz_content.split("\n")
+    index = 1
+    while index < len(content_parts):
+        question = content_parts[index - 1]
+        response = content_parts[index]
+        json_object = json.dumps({
+            "question": question[12:],
+            "response": response[12:]
+            }, indent=2
+        )
+        json_arr.append(json_object)
+        index += 3
+    return json_arr
+
+
 
 # def main():
-#     questions, responses = process_response("response.txt")
-#     for key in questions.keys():
-#         print(questions[key])
-#         print(responses[key])
-
+#     with open("response.txt", "r") as file:
+#         content = file.read()
+#         json_arr = process_quiz(content)
+#         for json_o in json_arr:
+#             print(json_o)
 # main()
