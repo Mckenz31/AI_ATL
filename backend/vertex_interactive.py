5492f79828c05651020c76e1720aed59a0048408
moisedk <moisedete@gmail.com>
2023-11-18 18:14:16 -0500

    Change formatting for vertex output

--- backend/vertex_interactive.py
+++ backend/vertex_interactive.py
@@ -1,6 +1,4 @@
-import pandas as pd
 import vertexai
-import json
 from vertexai.preview.language_models import (ChatModel, InputOutputTextPair,
                                               TextEmbeddingModel,
                                               TextGenerationModel)
@@ -12,47 +10,48 @@ def generate_open_questions(genModel: TextEmbeddingModel, lecture: str, number_o
     response = genModel.predict(
         f"""Background: You are a college professor who gave this lecture : {lecture}\
             Generate {number_of_quiz} or less quiz questions based on this lecture.\
-            Give the questions as a list of JSON objects in the format: [{"question: Question"}]\
-            For example, [{"question: What is an electron valence"},\
-            {"question: What is the logarithm base 2 of 8?"}].\
+            Give well elaborated questions, one question per line. No extra formatting, just the questions
             """,
         temperature=0,
         max_output_tokens=1024,
         top_k=1,
         top_p=1,
     )
-    return response.text
+    questions:str = response.text
+    return questions.split("\n")
 
 def generate_flash_cards(genModel: TextEmbeddingModel, lecture: str, number_of_cards: int = 20):
     response = genModel.predict(
         f"""Background: You are a college professor who gave this lecture : {lecture}\
             Generate {number_of_cards} or less flash card content based on this lecture.\
-            Give the flash cards content as a list of JSON objects in the format: [{"card: Card term, explanation: Explanation"}]\
-            For example, [{"card: Atom, response: The central part of the atom, containing protons and neutrons"},\
-            {"card: mars, explanation: A planet in the solar system"}].\
+            Give the flash cards content in the format "question : answer / explanation", each on a new line
+            For example, 
+            What is the third planet of the solar system : Earth
+            Which god is the most powerful god of Greek mytholody : Zeus
             """,
         temperature=0,
         max_output_tokens=1024,
         top_k=1,
         top_p=1,
     )
-    return response.text
+    return response.text.split("\n")
 def generate_mcq(genModel: TextEmbeddingModel, lecture: str, num_of_mcq: int = 20):
     response = genModel.predict(
         f"""Background: You are a college professor who gave this lecture : {lecture}\
             Generate {num_of_mcq} or less multiple choice questions based on this lecture.\
-            Give the questions as a list of JSON objects in the format: [{"question: Question, choices:[], answer: Answer"}]\
-            For example, [{"question: Which god is the most powerful in Greek mythology? [A) Hermes, B)Athena, C)Zeus, D) Apollo], answer: C)"},\
-            {"question: What is the logarithm base 2 of 8? [A) 3, B) 2, C) 0), D) 8, answer: A)"}].\
+            Give the questions in this format: Question : choices : answer
+            For example, Which god is the most powerful in Greek mythology? : A Hermes B Athena C Zeus D Apollo : C) \
+            Give one question per line
             """,
         temperature=0,
         max_output_tokens=1024,
         top_k=1,
         top_p=1,
     )
-    return response.text
+    questions:str = response.text
+    return questions.split("\n")
 
-def validate_answer(genModel: TextEmbeddingModel, lecture: str, answer: str, question):
+def validate_open_answer(genModel: TextEmbeddingModel, lecture: str, question: str, answer):
     response = genModel.predict(
         f"""Background: You are a college professor who gave this lecture : {lecture}\
             Based on the content in the lecture, is this answer: {answer} valid for the question: {question}? \
@@ -69,7 +68,15 @@ def main():
     genModel = TextGenerationModel.from_pretrained("text-bison@001")
     with open("sample_lecture.txt", "r") as file:
         lecture = file.read()
-        mcq = generate_mcq(genModel, lecture, 10)
+        questions = generate_open_questions(genModel, lecture, 10)
+        flashcards = generate_flash_cards(genModel, lecture, 16)
+        mcq = generate_mcq(genModel, lecture, 12)
+    print(questions)
+    print(flashcards)
     print(mcq)
+    question = "What is the third planet of the solar system"
+    answer = "Venus"
+    validation = validate_open_answer(genModel, lecture, question, answer)
+    print(validation)
 
 main()
