63762ef17db426915937d62cad55260f8a7a9657
qasema <qasema@berea.edu>
2023-11-18 20:48:54 -0500

    update QandA

--- backend/main.py
+++ backend/main.py
@@ -66,24 +66,15 @@ async def addFileToCloudStorage(request: Request):
 
 @app.get("/chatbot")
 def chatbot():
-   text = """
-Leo Messi: More Than Just a Soccer Player 
-
-Growing up in Argentina, soccer was a way of life. Every kid dreamed of being the next Diego Maradona and bringing glory to the national team. For me, that player was Lionel Messi. From a young age, Messi captured my imagination with his incredible skill on the ball and seemingly effortless ability to breeze past defenders. He played the game in a way I had never seen before. 
-
-Watching Messi weave through opposing players, I was inspired to go out to the concrete soccer field near my house and try to imitate his fancy footwork and creative dribbling. I would spend hours out there practicing Messi’s signature moves, trying to perfect that special connection between mind and body that allowed him to pull off his magical plays. Though I didn’t come close to achieving Messi’s mastery, his style of play opened my eyes to what was possible on the soccer field.
-
-Beyond the fancy tricks, what struck me most was Messi’s passion for the game. Even with his shy demeanor off the field, he played with sheer joy that was contagious to watch. Seeing the childlike grin on his face after scoring a goal, you could tell that he loved playing soccer simply for the sake of playing. At a time when many star athletes are accused of just being in it for the money and fame, Messi’s authentic love for soccer was special.
-
-   """
    text = get_text_from_storage(credentials,"ai-atl-transcriptions","transcription_train.txt") #Todo change this txt
    embeddings = chatBot(credentials,text.decode('utf-8'))
+   embeddings = chatBot(credentials,text)
    return embeddings
 # to add routes follow the format above
 
 @app.get("/askChatbot")
 def askchatbot():
-   question='what is code together about'
+   question= """what does the article say why bees can still fly?  """
    askQuestion(question,credentials)
    return askQuestion(question,credentials)
 
